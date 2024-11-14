2c803a6211b52eb6decec9e3f0ee7ae37070c259
Jealyang <jaelyangchoi@gmail.com>
2024-11-14 15:08:16 +0900

    feat: "기본 컨트롤러 생성"

---
+++ src/main/java/my/study/spring_security_jwt/controller/MainController.java
@@ -0,0 +1,15 @@
+package my.study.spring_security_jwt.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+    @GetMapping("/")
+    public String mainP() {
+
+        return "main Controller";
+    }
+}
