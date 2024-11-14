493e9aa31ac6bf4fcc8e55b5e7452f9f07b24a83
Jealyang <jaelyangchoi@gmail.com>
2024-11-14 16:32:40 +0900

    feat: "로그인 필터 구현"

--- src/main/java/my/study/spring_security_jwt/config/SecurityConfig.java
+++ src/main/java/my/study/spring_security_jwt/config/SecurityConfig.java
@@ -1,17 +1,31 @@
 package my.study.spring_security_jwt.config;
 
+import lombok.RequiredArgsConstructor;
+import my.study.spring_security_jwt.jwt.LoginFilter;
+import org.apache.catalina.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -34,6 +48,10 @@ public class SecurityConfig {
                         .requestMatchers("/login", "/", "/join").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
                         .anyRequest().authenticated());
+
+        http
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration))
+                        , UsernamePasswordAuthenticationFilter.class);
 
         //세션 설정
         http
